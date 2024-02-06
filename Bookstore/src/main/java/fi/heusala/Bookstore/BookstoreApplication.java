ae5e8a6018e2d75613c284e351cd8e69c41b9a0c
Ile83 <ilkka@heusala.fi>
2024-02-06 19:56:53 +0200

    Bookstore H2 tietokanta haku toiminnassa booklist.html

--- Bookstore/src/main/java/fi/heusala/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/fi/heusala/Bookstore/BookstoreApplication.java
@@ -1,7 +1,12 @@
 package fi.heusala.Bookstore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.heusala.Bookstore.domain.Book;
+import fi.heusala.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +15,29 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean // tämä puuttui, mutta en tiedä mitä se tekee (ajaa pavun sovelluksen
+			// käynnistyessä)
+	public CommandLineRunner createDemoData(BookRepository bookRepository) {
+		return (args) -> {
+			System.out.println("Luodaan demodataa H2-kantaan"); // h2-kanta on tietokanta, joka on käytössä
+			// tallennetaan muutama kirja Private string title, author, int publicationYear,
+			// String isbn, double price
+			bookRepository.save(new Book("Kirja1 ", "Kirjailija Kirjoittaja1", 2001, "ISBN1", 11));
+			bookRepository.save(new Book("Kirja2 ", "Kirjailija Kirjoittaja2", 2002, "ISBN2", 12));
+			bookRepository.save(new Book("Kirja3 ", "Kirjailija Kirjoittaja3", 2003, "ISBN3", 13)); // kehitysvaiheessa,
+																									// mutta ei
+																									// tuotannossa
+
+			System.out.println("Tulostetaan demodata Iden konsoliin");
+
+			for (Book book : bookRepository.findAll()) {
+				System.out.println(book.toString());
+
+				System.out.println("Kirjan tiedot: " + book.toString());
+
+			}
+
+		};
+
+	}
 }
--- Bookstore/src/main/java/fi/heusala/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/fi/heusala/Bookstore/web/BookController.java
@@ -1,11 +1,23 @@
 package fi.heusala.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import fi.heusala.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+
+    @Autowired // mikä tämä on?
+    private BookRepository bookRepository;
+
+    @GetMapping("/booklist")
+    public String showBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
 
     @RequestMapping("/index")
     public String hello(Model Model) {
